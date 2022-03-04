c6f6f450fdf5525db781855db8a2aaf0f248ff85
JosephRule <josephrule1@gmail.com>
2022-03-03 20:09:09 -0800

    generate scheduler html dynmically

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 var currentDayEl = document.querySelector("#currentDay")
+var scheduleContainerEl = document.querySelector("#scheduleContainer")
 
 var loadOrInitalizeHours = function() {
     schedule = JSON.parse(localStorage.getItem("schedule"))
@@ -13,16 +14,36 @@ var loadOrInitalizeHours = function() {
     return schedule;
 }
 
-var createHourRow = function(hour) {
-    // create a div with class row
-    // append div col with hour text
-    // append div col with
-    // append button
+var createHourRow = function(scheduleItem) {
+    console.log(scheduleItem.hour.format("hha"))
+    rowEl = document.createElement('div');
+    // add row id count with text so that we can update the saved array
+    rowEl.classList = "row";
+
+    hourEl = document.createElement("div");
+    // add function call to update to past, present, future class
+    hourEl.classList = "col-1 hour time-block";
+    hourEl.textContent = scheduleItem.hour.format("hha");
+    rowEl.appendChild(hourEl);
+
+    textboxEl = document.createElement("textarea");
+    textboxEl.classList = "col";
+    textboxEl.textContent = scheduleItem.text;
+    rowEl.appendChild(textboxEl);
+
+    buttonEl = document.createElement("button");
+    buttonEl.classList = "col-1 saveBtn bi bi-save";
+    saveEl = document.createElement("span");
+    saveEl.classList = 'oi oi-task';
+    buttonEl.appendChild(saveEl);
+    rowEl.appendChild(buttonEl);
+
+    scheduleContainerEl.appendChild(rowEl);
 }
 
 var createScheduler = function() {
     for (var i = 0; i<schedule.length; i++) {
-        createHourRow(schedule[i].hour)
+        createHourRow(schedule[i])
     }
 }
 
